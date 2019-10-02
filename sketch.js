31b23d76a4f760d0379109b5030e60feedf19c6b
zainiba <47758673+zainiba@users.noreply.github.com>
2019-10-01 17:42:46 -0700

    Added drawing box

--- sketch.js
+++ sketch.js
@@ -1,8 +1,27 @@
 function setup() {
   createCanvas(600, 600);
   background(200);
+  strokeWeight(15);
+  
 }
 
 function draw() {
 
+if (mouseIsPressed) {
+  stroke(map(mouseX, 0, 600, 0, 255, true));
+  line(mouseX, mouseY, pmouseX, pmouseY);
+  line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+
+}
+
+}
+
+function keyTyped() {
+  console.log(`key ${key} is beign typed`);
+
+  if (key === 's') {
+    //save this image
+    saveCanvas('fileName', 'png');
+  }
+  return false;
 }
