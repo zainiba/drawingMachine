5c26579b0b1226eea0360552e4bebf262f53d1a8
zainiba <47758673+zainiba@users.noreply.github.com>
2019-10-07 12:31:44 -0700

    Added noise function

--- sketch.js
+++ sketch.js
@@ -1,18 +1,25 @@
+let noiseOffset = 0.0;
+let strokeWidth = 15;
+
 function setup() {
   createCanvas(600, 600);
   background(200);
-  strokeWeight(15);
-  
+
 }
 
 function draw() {
+  strokeWeight(strokeWidth);
 
-if (mouseIsPressed) {
-  stroke(map(mouseX, 0, 600, 0, 255, true));
-  line(mouseX, mouseY, pmouseX, pmouseY);
-  line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+  noiseOffset += 0.05;
+  strokeWidth = noise(noiseOffset) * 50;
 
-}
+
+  if (mouseIsPressed) {
+    stroke(map(mouseX, 0, 600, 0, 255, true));
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+
+  }
 
 }
 
