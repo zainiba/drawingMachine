7966b70c5a65b1ac2967747bde4121b9db55fdd5
zainiba <47758673+zainiba@users.noreply.github.com>
2019-10-10 01:36:48 -0700

    Added loops and arrays

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 let noiseOffset = 0.0;
 let strokeWidth = 15;
+let gridArray = [];
+let array = [];
 
 function setup() {
   createCanvas(600, 600);
-  background(200);
-
+  drawGrid();
 }
 
 function draw() {
@@ -15,20 +16,48 @@ function draw() {
 
 
   if (mouseIsPressed) {
-    stroke(map(mouseX, 0, 600, 0, 255, true));
+    // stroke(map(mouseX, 0, 600, 0, 255, true));
     line(mouseX, mouseY, pmouseX, pmouseY);
-    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    array.push([mouseX, mouseY]);
 
   }
 
 }
 
 function keyTyped() {
-  console.log(`key ${key} is beign typed`);
-
   if (key === 's') {
     //save this image
     saveCanvas('fileName', 'png');
+  } else if (key === 'd') {
+    // display image
+    for (let i = 0; i < array.length - 1; i++) {
+       line(array[i][0], array[i],[1], array[i + 1][0], array[i + 1],[1]);
+    }
+
   }
   return false;
+}
+
+function drawGrid() {
+  numCells = 20
+  fillColor = 255;
+  noStroke();
+  for (let i = 0; i <= numCells; i += 1) {
+    gridArray[i] = [];
+    for (let j = 0; j <= numCells; j += 1) {
+      let x = i * width / numCells;
+      let y = j * height / numCells;
+
+      if (fillColor === 255) {
+        fillColor = 200;
+        gridArray[i][j] = 200;
+      } else {
+        fillColor = 255;
+        gridArray[i][j] = 255;
+      }
+      fill(fillColor);
+      rect(x, y, width / numCells, height / numCells)
+    }
+  }
+
 }
